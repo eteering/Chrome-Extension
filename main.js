bacb5711542dd923983106d8f241f980815a1577
eteering <teeringe@gmail.com>
2023-10-11 20:41:49 -0600

    next test

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+document.addEventListener('DOMContentLoaded', (e) => {
 // Select the image element using its ID
 const image = document.querySelectorAll('div > img');
 
 // Update the image source
-image.src = 'new-image.jpg';
+image.setAttribute('src', 'https://static.frontendmasters.com/assets/teachers/sentance/thumb@2x.jpg');
+});
